73bb9410a102e00636e48482f2dc9c8f3591456f
k-shunji <k-shunji@flect.co.jp>
2014-05-22 14:01:44 +0900

    Add room.cache.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,48 +4,65 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
  
-        // ファイル圧縮の設定
         copy: {
             src2sandbox: {
                 files: [{
-                    "expand": true,
-                    "cwd" : "src/",
-                    "src" : "**", 
-                    "dest" : "../room-sandbox/gh-pages/sample/javascripts/"
+                    expand: true,
+                    cwd : "src/",
+                    src : "**", 
+                    dest : "../room-sandbox/gh-pages/sample/javascripts/"
                 }]
             },
             dist2sandbox: {
                 files: [{
-                    "expand": true,
-                    "cwd" : "dist/",
-                    "src" : "**", 
-                    "dest" : "../room-sandbox/gh-pages/sample/javascripts/"
+                    expand: true,
+                    cwd : "dist/",
+                    src : "**", 
+                    dest : "../room-sandbox/gh-pages/sample/javascripts/"
                 }]
+            }
+        },
+
+        concat: {
+            dist : {
+                src : [
+                    "src/room.connection.js",
+                    "src/room.cache.js"
+                ],
+                dest: "dist/roomframework.js"
             }
         },
 
         uglify: {
             build: {
                 files: [{
-                    "dist/room.connection.min.js": "src/room.connection.js" 
+                    "dist/room.connection.min.js": "src/room.connection.js",
+                    "dist/room.cache.min.js": "src/room.cache.js",
+                    "dist/roomframework.min.js": "dist/roomframework.js"
                 }]
             }
         },
 
+        jshint : {
+            all : ['src/*.js']
+        },
+        
         watch: {
             files: [
                 'src/*.js'
             ],
-            tasks: ['copy', 'uglify']
+            tasks: ['jshint', 'concat', 'uglify', 'copy']
         }
     });
  
     // プラグインのロード
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
  
     // デフォルトタスクの設定
-    grunt.registerTask('default', [ 'uglify', 'copy']);
+    grunt.registerTask('default', [ 'concat', 'uglify', 'copy']);
  
 };
--- dist/roomframework.js
+++ dist/roomframework.js
@@ -180,3 +180,66 @@ $(function() {
 		});
 	};
 });
+if (typeof(room) === "undefined") room = {};
+
+$(function() {
+	room.Cache = function(storage, logErrors) {
+		function get(key) {
+			if (storage) {
+				try {
+					return storage.getItem(key);
+				} catch (e) {
+					//Ignore. Happened when cookie is disabled. 
+					if (logErrors && console) {
+						console.log(e);
+					}
+				}
+			}
+			return null;
+		}
+		function getAsJson(key) {
+			var ret = get(key);
+			if (ret) {
+				ret = JSON.parse(ret);
+			}
+			return ret;
+		}
+		function put(key, value) {
+			if (storage) {
+				if (typeof(value) === "object") {
+					value = JSON.stringify(value);
+				}
+				try {
+					storage.setItem(key, value);
+				} catch (e) {
+					//Ignore. Happened when cookie is disabled, or quota exceeded.
+					if (logErrors && console) {
+						console.log(e);
+					}
+				}
+			}
+		}
+		function keys() {
+			var ret = [];
+			if (storage) {
+				try {
+					for (var i=0; i<storage.length; i++) {
+						ret.push(storage.key(i));
+					}
+				} catch (e) {
+					//Ignore. Happened when cookie is disabled. 
+					if (logErrors && console) {
+						console.log(e);
+					}
+				}
+			}
+			return ret;
+		}
+		$.extend(this, {
+			"get" : get,
+			"getAsJson" : getAsJson,
+			"put" : put,
+			"keys" : keys
+		});
+	};
+});
